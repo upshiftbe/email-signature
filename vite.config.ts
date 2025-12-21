f649b57c352496f62661f65311cd76418a881b7a
tijsg <tijs.gommeren@gmail.com>
2025-12-21 20:29:35 +0100

    revert vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/email-signature/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
